402bd82e9b17a26e60203f277d7924ee71eab898
Q.A Rajpoot 007 <rqasahb@gmail.com>
2025-12-15 11:46:47 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Q.A Rajpoot',
+  author: '‎',
+  botName: "Q.A Rajpoot",
+  botOwner: 'Rajpoot007', // Your name
+  ownerNumber: '923704572227', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "private",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.5",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
